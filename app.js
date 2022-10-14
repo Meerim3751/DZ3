fd0a2e93a842a931ffca8251518e956f32754167
Meerim15 <meka3751@gmail.com>
2022-10-14 23:19:53 +0600

    first commit

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-var color = ["audi", "toyota", "lexus", "porsche", "bmw"]
-function carpark(string, array){
+var cars = ["audi", "toyota", "lexus", "porsche", "bmw"]
+function carspark(string, array){
     for(let i = 0; i < array.length; i ++){
         if(string == array[i]){
             return console.log(array[i], 'найден');
         }
     }  
 }
-console.log(carpark[3]) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var color = ["blue", "black", "pink", "aqua"]
-// function colorpick(string, array){
-//     for(let i = 0; i < array.length; i++){
-//         if(string == array[i]){
-//             return console.log(array[i], 'найден');
-//         }
-//     }  
-// }
-// console.log(colorpick[3]) 
+console.log(carspark[3]) 
